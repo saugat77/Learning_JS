79d84847809e8bde6a08e57b1e4ee3cb8f7c2714
Saugat <saugatpandey4@gmail.com>
2024-10-22 13:55:14 +0545

    successfully added currency converter

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import Counter from "./Counter.jsx";
 import React from 'react'
+import PasswordGenerator from './PasswordGenerator.jsx';
 
 const reactElement = React.createElement(
   'a',
@@ -10,8 +11,6 @@ const reactElement = React.createElement(
   'Click'
 )
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+createRoot(document.getElementById('root')).render( 
     <App />
-  </StrictMode>,
 )
